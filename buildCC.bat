83801d848cf8058e78bbcd32959ef3459e7e1fe6
alos-source <alexander.oswald@web.de>
2021-10-23 20:35:17 +0200

    create crc for build

--- buildCC.bat
+++ buildCC.bat
@@ -2,6 +2,7 @@ SET mypath=%~dp0
 SET CURDATE=%DATE:~6,4%-%DATE:~3,2%-%DATE:~0,2%
 SET sevenZip="C:\Program Files\Tools\7-Zip\7z.exe"
 
-%sevenZip% a -tzip ./Builds/{B5EE6DEF-00EB-4880-AC1E-39A6AF506363}.zip ./"{B5EE6DEF-00EB-4880-AC1E-39A6AF506363}"/*
-%sevenZip% a -tzip ./Builds/screenControl_%CURDATE%.zip ./Builds/{B5EE6DEF-00EB-4880-AC1E-39A6AF506363}.zip
+%sevenZip% a -tzip ./Builds/temp/{B5EE6DEF-00EB-4880-AC1E-39A6AF506363}.zip ./"{B5EE6DEF-00EB-4880-AC1E-39A6AF506363}"/*
+%sevenZip% h -scrcsha256 ./Builds/temp/{B5EE6DEF-00EB-4880-AC1E-39A6AF506363}.zip > ./Builds/temp/{B5EE6DEF-00EB-4880-AC1E-39A6AF506363}.CRC
+%sevenZip% a -tzip ./Builds/screenControl_%CURDATE%.zip ./Builds/temp/*
 
